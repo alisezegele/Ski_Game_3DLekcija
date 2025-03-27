d92f6a639d1ead03703d182553690fa9cf258c5d
alisezegele <146325058+alisezegele@users.noreply.github.com>
2025-03-27 13:20:39 +0200

    rocks and snowman

---
+++ Ski_Game_AZ/Assets/Scripts/Obstacle.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            PlayerCollision();
+        }
+    }
+
+    protected virtual void PlayerCollision()
+    {
+        Debug.Log("Player hit " + name);
+    }
+}
