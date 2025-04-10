9018cddc9f881f576c3e34b0ad981af063733922
erlens idk why tf does it say alise <alise.zeegele@gmail.com>
2025-04-10 13:19:03 +0300

    snowball + optimization

--- Ski_Game_AZ/Assets/Scripts/SnowmanThrow.cs
+++ Ski_Game_AZ/Assets/Scripts/SnowmanThrow.cs
@@ -8,37 +8,40 @@ public class SnowmanThrow : MonoBehaviour
     public float throwDistance;
     public int throwSpeed;
     private bool justThown = false;
+    private GameObject target;
+    [SerializeField] private Vector3 upwardsOffset = new Vector3(0, 0.33f, 0);
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        target = GameObject.Find("Player");
     }
-
-    // Update is called once per frame
     void Update()
     {
-       GameObject target = GameObject.Find("Player");
-       
-       float distanceToTarget = Vector3.Distance(target.transform.position, transform.position);
-
-        if (distanceToTarget < throwDistance&&justThown==false)
+        if (Time.frameCount % 5 == 0)
         {
-            justThown = true;
-            GameObject tempSnowBall = Instantiate(snowBall,transform.position,transform.rotation);
-            Rigidbody tempRb = tempSnowBall.GetComponent<Rigidbody>();
-            Vector3 targetDirection =  Vector3.Normalize(target.transform.position-transform.position);
+            float distanceToTarget = Vector3.Distance(target.transform.position, transform.position);
             
-            //Add a small throw angle
-            targetDirection += new Vector3(0, 0.33f, 0);
-            tempRb.AddForce(targetDirection * throwSpeed);
-            Invoke("ThrowOver", 0.1f);
+            if (distanceToTarget < throwDistance && justThown == false)
+            {
+                ThrowSnowball();
+            }
         }
-
     }
-
     void ThrowOver()
     {
         justThown = false;
+    }
+
+    void ThrowSnowball()
+    {
+        justThown = true;
+        GameObject tempSnowBall = Instantiate(snowBall, transform.position, transform.rotation);
+        Rigidbody tempRb = tempSnowBall.GetComponent<Rigidbody>();
+        Vector3 targetDirection = Vector3.Normalize(target.transform.position - transform.position);
+                
+        targetDirection += upwardsOffset;
+        tempRb.AddForce(targetDirection * throwSpeed);
+        Invoke("ThrowOver", 0.1f);
     }
 }
